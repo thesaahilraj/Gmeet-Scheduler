a9257c8f6ec9f811457c0d6e7c4c4688f1ab12dd
thesaahilraj <saahilraj@hotmail.com>
2020-10-21 17:31:14 +0530

    Update

--- GmeetScheduler.py
+++ GmeetScheduler.py
@@ -2,18 +2,20 @@ import streamlit as st
 import pyautogui
 import os
 import webbrowser as wb
-import datetime
+from datetime import datetime
 import time
 
 '''
 Welcome to G-Meet Scheduler
 '''
 
-now = datetime.time()
-date_today = datetime.date.today()
 
-start_time = st.time_input('Enter the Meeting Time')
-start_date = st.date_input('Enter the Meeting Date')
+st.text('Enter the Meeting Time')
+hour = st.slider('Enter Hour : ', min_value=00, max_value=23)
+minute = st.slider('Enter Minute: ', min_value=00, max_value=59)
+seconds = st.slider('Enter Seconds: ', min_value=00, max_value=59)
+sched_time = str(hour) + str(minute) + str(seconds)
+
 meet_code = st.text_input('Enter the Meeting Code')
 
 chromepath = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
@@ -22,15 +24,13 @@ button = st.button(' Schedule Now! ')
 if button:
     st.text('Scheduled Successfully!')
     while True:
-
-        if (start_time == now and start_date == date_today):
-            wb.open('http://www.https://meet.google.com/' + meet_code)
-            time.sleep(10)
-            pyautogui.keyDown('ctrl')
-            pyautogui.press('d')
-            time.sleep(1)
-            pyautogui.press('e')
-            pyautogui.keyUp('ctrl')
+        noww = datetime.now()
+        if noww.strftime("%H%M%S") == sched_time:
+            wb.open('https://meet.google.com/' + meet_code)
+            time.sleep(20)
+            pyautogui.hotkey('ctrl', 'd')
+            time.sleep(5)
+            pyautogui.hotkey('ctrl', 'e')
 
             time.sleep(3)
             pyautogui.click(1452, 582)  # Exact 'JOIN NOW' Coordinates
